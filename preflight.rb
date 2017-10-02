7cdfa33474d65b062ba8ee929162f3c19d88ea7f
sensoph1 <sensoph@gmail.com>
2017-10-02 14:32:32 -0500

    initial

---
+++ preflight.rb
@@ -0,0 +1,15 @@
+require 'preflight'
+input_array = ARGV
+preflight = Preflight::Profiles::PDFX1A.new
+
+#first_arg, *the_rest = ARGV
+filename = input_array[0].to_s
+#first_art.to_s
+output =  preflight.check(filename).inspect
+
+if (output.length <3)
+
+	puts "no errors"
+else
+	puts output
+end
